e4460decfff5c05ab64a77b2a1590f780e042cc1
JoamirS <jmrsilveira@outlook.com>
2019-07-04 21:15:25 -0300

    Hello World e Tipos de dados

---
+++ 01class.py
@@ -0,0 +1,31 @@
+'''
+    Hello World e Tipos de dados
+'''
+
+print('Hello, World')
+
+
+class TestProgram:
+    a = 10
+    b = 11
+    print(a + b)
+
+
+a = 10
+print(type(a))
+
+b = 10.3
+print(type(b))
+
+c = 'String'
+print(type(c))
+
+d = ['Item1', 'Item2', 'Item3']
+print(type(d))
+
+e = ('Item1', 'Item2', 'Item3')
+print(type(e))
+
+f = {1: 'Fulano', 2: 'Beltrano'}
+print(type(f))
+
