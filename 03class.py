5276c5cc80a70216bdcb866533f04fa3ebd365e8
JoamirS <jmrsilveira@outlook.com>
2019-07-04 21:26:09 -0300

    While condition
    Estrutura de repetição While.

---
+++ 03class.py
@@ -0,0 +1,12 @@
+'''
+    Estrutura de repetição While.
+'''
+
+# Fiz um loop com a condição verdadeira, mas que após a primeira tentativa se torna falsa, e para a execução.
+condition = True
+
+while condition:
+    print('Execute')
+    condition = False
+else:
+    print('Fim do loop')
