61f0126a7de2be1b0919420397423fc8e5e298a4
JoamirS <jmrsilveira@outlook.com>
2019-07-04 21:26:34 -0300

    Estrutura de repetição for

---
+++ 05class.py
@@ -0,0 +1,9 @@
+'''
+    Estrutura de repetição for
+'''
+
+#   O quatro siginifica de onde parte minha contagem,
+for NumberCount in range(4, 10):
+    print(NumberCount)
+else:
+    print('Fim do loop')
