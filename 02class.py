c1ce4b083fee2309748fda1abbfc9c7aa92b2999
JoamirS <jmrsilveira@outlook.com>
2019-07-04 21:24:51 -0300

    If and Else

---
+++ 02class.py
@@ -0,0 +1,14 @@
+'''
+    Testando o if e else
+'''
+
+NumberOne = int(input('Digite o primeiro número: '))
+NumberTwo = int(input('Digite o segundo número: '))
+
+if NumberOne > NumberTwo:
+    print('O primeiro número é maior que o segundo.')
+elif NumberTwo == NumberOne:
+    print('Os dois números são iguais.')
+else:
+    print('O primeiro número é menor que o primeiro')
+
