dd5006dad8c99b8f584a575c920197febfabba52
JoamirS <jmrsilveira@outlook.com>
2019-07-04 21:29:18 -0300

    Operadores aritiméticos

---
+++ 09class.py
@@ -0,0 +1,43 @@
+'''
+    Operadores aritiméticos
+'''
+
+# Declarando as variáveis pela primeira vez
+VariableA = 11
+VariableB = 4
+
+# Soma
+VariableSum = VariableA + VariableB
+print('O resultado da soma das variáveis A e B é {}'.format(VariableSum))
+
+# Subtração
+VariableSubtraction = VariableA - VariableB
+print('O resultado da subtração das variáveis A e B é {}'.format(VariableSubtraction))
+
+# Multiplicação
+VariableMultiplication = VariableA * VariableB
+print('O resultado da multiplicação das variáveis A e B é {}'.format(VariableMultiplication))
+
+# Divisão
+VariableDivision = VariableA / VariableB
+print('O resultado da divisão das variáveis A e B é {}'.format(VariableDivision))
+
+# Resto da divisão
+VariableModulo = VariableA % VariableB
+print('O módulo da divisão das variáveis A e B é {}'.format(VariableModulo))
+
+# ------------------------------------------------------------------------------------------------------------------
+
+# Declarando as variáveis pela segunda vez
+VariableC = 3
+VariableD = 4
+#                      | Base |   | Expoente |
+VariablePotentiation = VariableC ** VariableD
+print('O resultado de 3 elevado a 4 potência é {}'.format(VariablePotentiation))
+
+# ------------------------------------------------------------------------------------------------------------------
+VariableF = 7
+VariableG = 8
+
+VariableDivisionInteger = VariableF // VariableG
+
