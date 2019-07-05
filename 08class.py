71716b962252fa9c206f9a70b05f27ea77bda64f
JoamirS <jmrsilveira@outlook.com>
2019-07-04 21:29:09 -0300

    Operadores lógicos
    Obs: Essas funções serão melhor desenvolvidas no Shell do python3.

---
+++ 08class.py
@@ -0,0 +1,30 @@
+'''
+    Operadores lógicos
+    Obs: Essas funções serão melhor desenvolvidas no Shell do python3.
+'''
+
+VariableOne = True
+VariableTwo = False
+
+VariableOne and VariableTwo
+
+VariableOne or VariableTwo
+
+(VariableOne and VariableTwo) or (VariableOne or VariableTwo)
+
+VariableA = 10
+VariableB = 20
+
+VariableA > VariableB
+
+VariableB < VariableA
+
+VariableA >= VariableB
+
+VariableA <= VariableB
+
+VariableA == VariableB
+
+curso = 'Programe rápido'
+'Programe' in curso
+'teste' not in curso
