b55e9064c67c5d36addcac3f2f2edd93dd5a6c4a
JoamirS <jmrsilveira@outlook.com>
2020-04-29 11:49:49 -0300

    Main program

---
+++ jogos/jogos.py
@@ -0,0 +1,16 @@
+import advinhacao
+import forca
+
+print('-' * 35)
+print("Escolha o seu jogo")
+print('-' * 35)
+
+print('Opção 1: Forca | Opção 2: Advinhação ')
+option_game_user = int(input('Qual jogo você deseja? '))
+
+if option_game_user == 1:
+    print('Jogo da forca escolhido')
+    forca.play()
+elif option_game_user == 2:
+    print('Jogo da Advinhação escolhido')
+    advinhacao.play()
