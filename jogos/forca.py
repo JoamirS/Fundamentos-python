d4ca9e571775c7d591cd7ce1ba9eb83a4c0b1dd0
JoamirS <jmrsilveira@outlook.com>
2020-05-08 11:26:48 -0300

    Scrolling through the string and finding the letter position

--- jogos/forca.py
+++ jogos/forca.py
@@ -3,6 +3,22 @@ def play():
     print("Bem vindo ao jogo da Forca")
     print('-' * 35)
 
+    word_secret = "Minions"
+    hanged = False
+    correct_word = False
+
+    while not hanged and not correct_word:
+        attempt_user = input('Qual letra deseja procurar? ')
+        attempt_user = attempt_user.strip()
+
+        index_word = 0
+        for word in word_secret:
+            if attempt_user.upper() == word.upper():
+                print(f'Encontrei a letra {word} na posição {index_word}')
+            index_word += 1
+
+        print('Jogando')
+
     print("Fim do jogo")
 
 
