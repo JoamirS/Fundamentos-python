7520e0f4d50bfc19109d9fdab28954e57d75e93b
JoamirS <jmrsilveira@outlook.com>
2020-05-30 22:52:35 -0300

    Challenge list 3

---
+++ jogos/Challenge_list_3.py
@@ -0,0 +1,11 @@
+"""
+    Suponha que tenhamos uma lista com os seguintes inteiros:
+    inteiros = [1,3,4,5,7,8]
+
+    Podemos preencher uma nova lista com o quadrado de cada número da lista anterior, através do recurso List Comprehension.
+    Considerando que, para calcular o quadrado de um número, fazemos x*x , qual seria o código para obter a lista de quadrados?
+"""
+
+integer_list = [1, 3, 4, 5, 7, 8]
+list_quadratic = [number_integer * number_integer for number_integer in integer_list]
+print(list_quadratic)
