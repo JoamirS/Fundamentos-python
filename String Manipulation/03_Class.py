dbca0cc1dc0d10ccadd6e533b2d67045eeffab28
JoamirS <jmrsilveira@outlook.com>
2021-02-10 10:06:50 -0300

    main file

--- String Manipulation/03_Class.py
+++ String Manipulation/03_Class.py
@@ -1,3 +1,12 @@
 class ExtratorArgumentoUrl:
     def __init__(self, url):
-        self.url = url
+        if self.url_e_valida(url):
+            self.url = url
+        else:
+            raise LookupError("Url Inválida!")
+
+    def url_e_valida(self, url):
+        if url:
+            return True
+        else:
+            return False
