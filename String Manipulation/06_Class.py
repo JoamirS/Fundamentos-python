f974381728c3865050164e0924411e5149e8eabc
JoamirS <jmrsilveira@outlook.com>
2021-02-13 09:37:04 -0300

    Method format

---
+++ String Manipulation/06_Class.py
@@ -0,0 +1,19 @@
+'''
+    É possível utilizar o format para alinhar os textos e para determinar uma quantidade máxima de caracteres que podem ser exibidos. Veja a lista abaixo com alguns exemplos e seus resultados para a seguinte string: texto = “Eu gosto de pizza de {}”.format(“Calabresa”).
+
+{0:>20} - Alinha a string à direita com 20 espaços em branco
+
+Resposta : Eu gosto de pizza de Calabresa
+
+{0:#>20} - Alinha a string à direita com 20 símbolos #
+
+Resposta : Eu gosto de pizza de ###########Calabresa
+
+{0:#^20} - A linha a string no centro de 20 símbolos #.
+
+Resposta : Eu gosto de pizza de #####Calabresa######
+
+{0:.5} - Imprime somente os 5 primeiros dígitos
+
+Resposta : Eu gosto de pizza de Calab
+'''
