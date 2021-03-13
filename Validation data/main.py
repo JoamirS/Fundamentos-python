c6e79442c11b46df62e5cf2bbd20ec70e0f27d3e
JoamirS <jmrsilveira@outlook.com>
2021-03-12 23:32:34 -0300

    Test cpf

--- Validation data/main.py
+++ Validation data/main.py
@@ -3,8 +3,8 @@ from cpf_and_cnpj import Document
 test_cpf = "12354367996"
 test_cnpj = "15436940000103"
 
-document_cnpj = Document.create_document(test_cnpj)
+#document_cnpj = Document.create_document(test_cnpj)
 document_cpf = Document.create_document(test_cpf)
 
-print(document_cnpj)
+#print(document_cnpj)
 print(document_cpf)
