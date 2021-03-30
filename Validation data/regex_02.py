3ac2ad20f0c171a6fb27ed9d41c4fbaaf77e6655
Joamir Silveira <jmrsilveira@outlook.com>
2021-03-30 14:35:26 -0300

    Regex with |

--- Validation data/regex_02.py
+++ Validation data/regex_02.py
@@ -1,8 +1,8 @@
 import re
 
 texto = '''
-João trouxe    flores para sua amada namorada em 10 de janeiro de 1970,
-Maria era o nome dela.
+João trouxe flores para sua amada namorada em 10 de janeiro de 1970,
+maria era o nome dela.
 Foi um ano excelente na vida de joão. Teve 5 filhos, todos adultos atualmente.
 maria, hoje sua esposa, ainda faz aquele café com pão de queijo nas tardes de
 domingo. Também né! Sendo a boa mineira que é, nunca esquece seu famoso
@@ -12,3 +12,7 @@ Não canso de ouvir a Maria:
 '''
 
 print(re.findall(r'João | Maria', texto))
+
+print(re.findall(r'João | Maria | q..ijo', texto))
+
+print(re.findall(r'[Jj]oão|[Mm]aria', texto))
